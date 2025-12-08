39502d73981d570c72c86e95d75cfb10659917da
hiiisiii <8243.hk@gmail.com>
2025-12-08 09:24:07 +0000

    sprint3

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -1,12 +1,14 @@
 // prisma/prisma.js
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import prismaPkg from "@prisma/client";
+const { PrismaClient } = prismaPkg;
+
 import { PrismaPg } from "@prisma/adapter-pg";
 import pkg from "pg";
 
+const { Pool } = pkg;
 const connectionString = process.env.DATABASE_URL;
-const pool = new pkg.Pool({ connectionString });
-
+const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({
